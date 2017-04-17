29f6d3fdfc527c6fa910dd7c814c6a0b46b78ecd
Jacob Landman <jakeypoolandman7@gmail.com>
2017-04-17 12:58:03 -0500

    finished strings lesson

---
+++ javaScriptBasics/strings.js
@@ -0,0 +1,29 @@
+// Strings are a data type used to represent text and are wrapped in either a
+// single or double quotes.
+
+// Example
+// Single quote
+let name = 'Jacob';
+console.log(name);
+
+// Double quotes
+name = "Jane";
+console.log(name);
+
+// Error
+// name = 'Jack";
+
+let message = "It's nice to see you!";
+console.log(message);
+
+// error
+// message = 'It's nice to see you!'';
+
+// backslash is an escape character
+message = 'It\'s nice to see you!';
+console.log(message);
+
+console.log(typeof 942); // Number
+console.log(typeof '942'); // String
+console.log(typeof true); // Boolean
+console.log(typeof 'true'); // String
