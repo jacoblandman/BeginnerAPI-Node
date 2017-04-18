85bd9612e3a5dbb2508e40041963680391c9d8fe
Jacob Landman <jakeypoolandman7@gmail.com>
2017-04-18 14:03:57 -0500

    Added GET request

--- course-api/src/controller/restaurant.js
+++ course-api/src/controller/restaurant.js
@@ -5,8 +5,10 @@ import Restaurant from '../model/restaurant';
 export default({ config, db }) => {
   let api = Router();
 
+  // CRUD - Create Read Update Delete
   // specifying a version like 'v1' makes it easy to add a 'v2' without breaking 'v1'
-  // 'v1/restaurant/add'
+
+  // 'v1/restaurant/add' - Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -19,6 +21,29 @@ export default({ config, db }) => {
       res.json({ message: 'Restaurant saved successfully' });
     });
   });
+
+  // '/v1/restaurant' - READ
+  api.get('/', (req, res) => {
+    // tell mongoose to find everything
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // 'v1/restaurant/:id' - Read 1
+  api.get('/:id', (req, res) => {
+    // tell mongoose to find by id
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
 
   return api;
 }
